ceb1b0f33c290da4c6a79d262b35f6087b33ed5e
Alex <linuxoidoz@gmail.com>
2020-06-24 11:40:50 +1100

    Add files via upload

--- js/ab_package_skeleton_dashboard.js
+++ js/ab_package_skeleton_dashboard.js
@@ -1,11 +1,23 @@
 /**
- * Business Listing package for Concrete5
+ * Package Skeleton package for Concrete5
+ * Copyright 2020, Alex Borisov. All Rights Reserved.
  *
- * concrete5.org marketplace license
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * 
  * @author Alex Borisov <linuxoidoz@gmail.com>
- * @copyright 2018-2019, Alex Borisov
- * @package Concrete\Package\ab_business_listing
+ * @copyright 2020, Alex Borisov. All Rights Reserved.
+ * @package Concrete\Package\ab_package_skeleton
  */
 
 $(function(){
